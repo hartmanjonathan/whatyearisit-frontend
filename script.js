c03842d533655b4bbd044cafa5ee8f269dae60b1
HARTMAN <jonathan.raissa@outlook.fr>
2023-11-03 16:17:17 +0100

    first commit

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const getYearFromBackend = () => {
   fetch('https://whatyearisit-backend-h3zpjxtey-hartmans-projects.vercel.app/year')
  .then(response => response.json())
  .then(data => {
-   document.querySelector('#year').textContent = data.year;
+   document.queryElemendById('year').textContent = data.year;
  });
 };
 
-// Appel de la fonction pour récupérer l'année
-getYearFromBackend();
