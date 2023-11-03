1755f43abaf0b7c9d7d676a4adce811f26d5d23b
HARTMAN <jonathan.raissa@outlook.fr>
2023-11-03 16:45:32 +0100

    fix errr

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Fonction pour récupérer l'année depuis le backend hébergé sur Vercel
 const getYearFromBackend = () => {
-  fetch('https://whatyearisit-backend-h3zpjxtey-hartmans-projects.vercel.app/year')
+  fetch('https://whatyearisit-navy.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.queryElemendById('year').textContent = data.year;
